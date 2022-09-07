443c8e780b1d4b9fe43708b81ce3af3b0994405c
Zachary Hunt <huntzachary30@gmail.com>
2022-09-07 11:53:25 -0500

    Fixed Overtime.cs to check for all test cases.

--- CSharp-Challenge-OvertimePay/classes/OverTime.cs
+++ CSharp-Challenge-OvertimePay/classes/OverTime.cs
@@ -6,6 +6,7 @@ namespace CSharp_Challenge_OvertimePay.classes
         // - Fields -
         private double[] workingDay; //Format for array goes [Start of Day (Decimal), End of Day (Decimal), Hourly Rate, Overtime Multiplier] | Time in 24hr Format
         private double startDay, endDay, hourlyRate, overtimeMultiplier;
+        private bool overtimeWorked;
 
         // - Constructor -
         public OverTime(double[] workingDay)
@@ -15,27 +16,28 @@ namespace CSharp_Challenge_OvertimePay.classes
             this.endDay = workingDay[1];
             this.hourlyRate = workingDay[2];
             this.overtimeMultiplier = workingDay[3];
+            this.overtimeWorked = false;
         }
 
 
         public double CalculatePay()
         {
-            double hoursWorked = this.endDay - this.startDay; //Takes difference of hours to see how many hours employee worked
             double overtimeHours = 0, regularHours = 0;
 
             //Checks if employee worked after 5PM | Calculates hours of overtime
             if (this.endDay > 17) 
             {
                 overtimeHours = this.endDay - 17;
+                this.overtimeWorked = true;
             }
 
-            //Checks for 9AM - 5PM | Regular hours
-            if (this.startDay >= 9) 
+            //Checks for 9AM - 5PM | Regular hours w/ Overtime
+            if (this.startDay >= 9 && overtimeWorked) 
             {
                 regularHours = 17 - this.startDay;
-            } else
+            } else //Regular Hours w/o Overtime
             {
-                Console.WriteLine("WARNING: Employee started before 9AM.");
+                regularHours = this.endDay - this.startDay; //Calculates regular hours during normal working hours
             }
 
             return (regularHours * this.hourlyRate) + (overtimeHours * this.hourlyRate * this.overtimeMultiplier);
--- CSharp-Challenge-OvertimePay/classes/Program.cs
+++ CSharp-Challenge-OvertimePay/classes/Program.cs
@@ -1,7 +1,11 @@
 ﻿using CSharp_Challenge_OvertimePay.classes;
 // See https://aka.ms/new-console-template for more information
 
-OverTime overtime = new OverTime(new double[] { 9, 17, 30, 1.5 });
+OverTime overtime = new OverTime(new double[] { 9, 17, 30, 1.5 }); //Expected output $240.00
 Console.WriteLine($"${overtime.CalculatePay().ToString("#.00")}");
-OverTime overtime2 = new OverTime(new double[] { 16, 18, 30, 1.8 });
+
+OverTime overtime2 = new OverTime(new double[] { 16, 18, 30, 1.8 }); //Expected output $54.00
 Console.WriteLine($"${overtime2.CalculatePay().ToString("#.00")}");
+
+OverTime overtime3 = new OverTime(new double[] { 13.25, 15, 30, 1.5 });
+Console.WriteLine($"${overtime3.CalculatePay().ToString("#.00")}"); //Expected output $52.50
