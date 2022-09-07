526d7b197a11bc365aab9f4b5a35f42ee6702b12
Zachary Hunt <huntzachary30@gmail.com>
2022-09-07 09:26:19 -0500

    Added Overtime.cs

---
+++ CSharp-Challenge-OvertimePay/classes/OverTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CSharp_Challenge_OvertimePay.classes
+{
+    public class OverTime
+    {
+        private double[] workingDay; //Format for array goes [Start of Day (Decimal), End of Day (Decimal), Hourly Rate, Overtime Multiplier] | Time in 24hr Format
+
+        public OverTime(double[] workingDay)
+        {
+            this.workingDay = workingDay;
+        }
+    }
+}
+
--- CSharp-Challenge-OvertimePay/classes/Program.cs
+++ CSharp-Challenge-OvertimePay/classes/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharp_Challenge_OvertimePay.classes;
+// See https://aka.ms/new-console-template for more information
 
+OverTime overtime = new OverTime(new double[] { 9, 17, 30, 1.5 });
