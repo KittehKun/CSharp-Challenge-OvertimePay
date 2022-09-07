bb459ab8ece78c18c41282320af268399f6288eb
Zachary Hunt <huntzachary30@gmail.com>
2022-09-07 12:00:49 -0500

    Changed logging statements.

--- CSharp-Challenge-OvertimePay/classes/Program.cs
+++ CSharp-Challenge-OvertimePay/classes/Program.cs
@@ -2,10 +2,10 @@
 // See https://aka.ms/new-console-template for more information
 
 OverTime overtime = new OverTime(new double[] { 9, 17, 30, 1.5 }); //Expected output $240.00
-Console.WriteLine($"${overtime.CalculatePay().ToString("#.00")}");
+Console.WriteLine($"Earned ${overtime.CalculatePay().ToString("#.00")} today!");
 
 OverTime overtime2 = new OverTime(new double[] { 16, 18, 30, 1.8 }); //Expected output $54.00
-Console.WriteLine($"${overtime2.CalculatePay().ToString("#.00")}");
+Console.WriteLine($"Earned ${overtime2.CalculatePay().ToString("#.00")} today!");
 
 OverTime overtime3 = new OverTime(new double[] { 13.25, 15, 30, 1.5 });
-Console.WriteLine($"${overtime3.CalculatePay().ToString("#.00")}"); //Expected output $52.50
+Console.WriteLine($"Earned ${overtime3.CalculatePay().ToString("#.00")} today!"); //Expected output $52.50
